44a2bbc26e66850657ec3c5370f288bae9723128
Joshua Zhuchenko <105894219+jzhuchenko2@users.noreply.github.com>
2024-02-12 14:11:27 -0600

    rotation speed w/ #

--- visualizecode.py
+++ visualizecode.py
@@ -5,5 +5,5 @@ int main() {
     // Open a file for writing position data
     std::ofstream outfile("output.dat");
 
-                    
+   const double rotationSpeed = 1.0e-6;                 
                   }
