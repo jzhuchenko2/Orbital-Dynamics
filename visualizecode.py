1162724f5fb96f7dd66a33a8ab2eadf40c2aa0cd
Joshua Zhuchenko <105894219+jzhuchenko2@users.noreply.github.com>
2024-02-16 01:20:12 -0600

    integrate

--- visualizecode.py
+++ visualizecode.py
@@ -13,5 +13,6 @@ const double dt = 1.0;                   // time step in seconds
 Spacecraft spacecraft(1e6, 0, 1e4, 0, 2e4, 0);  // Placed at (x=1e6, y=0, z=1e4) with initial velocity in y-direction
 
 //performing simulation
-for (double t = 0; t <= simulationTime; t += dt)
+for (double t = 0; t <= simulationTime; t += dt) {
+        integrate(spacecraft, rotationSpeed, dt);
                   }
