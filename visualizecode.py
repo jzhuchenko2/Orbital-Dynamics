db2d67afd877986af80c8d05df8d2ba112b7a86b
Joshua Zhuchenko <105894219+jzhuchenko2@users.noreply.github.com>
2024-02-16 01:20:39 -0600

    POSITIONS

--- visualizecode.py
+++ visualizecode.py
@@ -15,4 +15,6 @@ Spacecraft spacecraft(1e6, 0, 1e4, 0, 2e4, 0);  // Placed at (x=1e6, y=0, z=1e4)
 //performing simulation
 for (double t = 0; t <= simulationTime; t += dt) {
         integrate(spacecraft, rotationSpeed, dt);
+
+    spacecraft.position = newmanJanis(spacecraft.position, rotationSpeed);
                   }
