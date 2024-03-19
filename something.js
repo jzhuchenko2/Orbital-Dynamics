dab4035b06fc9206f1d9039577e06ae8ac16c913
Joshua Zhuchenko <105894219+jzhuchenko2@users.noreply.github.com>
2024-03-18 23:07:31 -0500

    Update something.js

--- something.js
+++ something.js
@@ -3,3 +3,9 @@ const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerH
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
+
+const material = new THREE.ShaderMaterial({
+    vertexShader: vertexShaderSource,
+    fragmentShader: fragmentShaderSource,
+});
+//this would have to work w/ some glsl code to work.
